89dcd8ed11a0dbdeaa3a96353176771ebd38ddf7
aleksei <darklisik.darklisik@yandex.ru>
2025-02-06 17:38:45 +0300

    add method receive(Content) into BotCommandHandler.java

--- src/main/java/ru/job4j/handler/BotCommandHandler.java
+++ src/main/java/ru/job4j/handler/BotCommandHandler.java
@@ -1,7 +1,12 @@
 package ru.job4j.handler;
 
 import org.springframework.stereotype.Service;
+import ru.job4j.model.Content;
 
 @Service
 public class BotCommandHandler {
+
+    void receive(Content content) {
+        System.out.println(content);
+    }
 }
