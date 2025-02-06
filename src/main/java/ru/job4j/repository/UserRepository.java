ec2a2334c345f4d71312fa1ccfcdf2a0584158c0
aleksei <darklisik.darklisik@yandex.ru>
2025-02-06 21:27:21 +0300

    create new repo interface: UserRepository.java

--- src/main/java/ru/job4j/repository/UserRepository.java
+++ src/main/java/ru/job4j/repository/UserRepository.java
@@ -1,4 +1,14 @@
 package ru.job4j.repository;
 
-public class UserRepository {
+import ru.job4j.model.User;
+
+import java.util.Collection;
+
+public interface UserRepository {
+
+    User save(User user);
+
+    User findByClientId(Long clientId);
+
+    Collection<User> findAll();
 }
