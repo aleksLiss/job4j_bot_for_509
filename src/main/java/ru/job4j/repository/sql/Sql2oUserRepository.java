333f5593a8814d351bf39a774abdd8e55d9f89e1
aleksei <darklisik.darklisik@yandex.ru>
2025-02-09 16:25:30 +0300

    add logger into Sql2oUserRepository.java

--- src/main/java/ru/job4j/repository/sql/Sql2oUserRepository.java
+++ src/main/java/ru/job4j/repository/sql/Sql2oUserRepository.java
@@ -1,5 +1,7 @@
 package ru.job4j.repository.sql;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import org.sql2o.Connection;
 import org.sql2o.Query;
@@ -8,12 +10,14 @@ import ru.job4j.model.User;
 import ru.job4j.repository.UserRepository;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public class Sql2oUserRepository implements UserRepository {
 
     private final Sql2o sql2o;
+    private static final Logger LOGGER = LoggerFactory.getLogger(Sql2oUserRepository.class);
 
     public Sql2oUserRepository(Sql2o sql2o) {
         this.sql2o = sql2o;
@@ -28,26 +32,35 @@ public class Sql2oUserRepository implements UserRepository {
                     .addParameter("lastName", user.getLastName());
             int generatedId = sql.executeUpdate().getKey(Integer.class);
             user.setId(generatedId);
-            return user;
+        } catch (Exception ex) {
+            LOGGER.error(ex.getMessage());
         }
+        return user;
     }
 
     @Override
     public Optional<User> findByClientId(Long clientId) {
+        User foundUser = null;
         try (Connection connection = sql2o.open()) {
             Query sql = connection.createQuery("SELECT * FROM users WHERE client_id = :clientId")
                     .addParameter("clientId", clientId);
-            User foundUser = sql.executeAndFetchFirst(User.class);
-            return Optional.ofNullable(foundUser);
+            foundUser = sql.executeAndFetchFirst(User.class);
+        } catch (Exception ex) {
+            LOGGER.error(ex.getMessage());
         }
+        return Optional.ofNullable(foundUser);
     }
 
     @Override
     public Collection<User> findAll() {
+        Collection<User> users = List.of();
         try (Connection connection = sql2o.open()) {
             Query sql = connection.createQuery("SELECT * FROM users");
-            return sql.setColumnMappings(User.COLUMN_MAPPING).executeAndFetch(User.class);
+            users = sql.setColumnMappings(User.COLUMN_MAPPING).executeAndFetch(User.class);
+        } catch (Exception ex) {
+            LOGGER.error(ex.getMessage());
         }
+        return users;
     }
 
     @Override
@@ -56,6 +69,8 @@ public class Sql2oUserRepository implements UserRepository {
             Query sql = connection.createQuery("DELETE FROM users WHERE client_id = :clientId")
                     .addParameter("clientId", clientId);
             sql.executeUpdate();
+        } catch (Exception ex) {
+            LOGGER.error(ex.getMessage());
         }
     }
 }
